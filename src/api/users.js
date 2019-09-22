72ce9c20abbc62b6ff53030a06f7cb72618da3ed
Daniel Augusto de Melo Santos <danielmelos@gmail.com>
2019-09-22 15:34:15 -0300

    Fixed authorizers to new endpoints

--- src/api/users.js
+++ src/api/users.js
@@ -1,5 +1,13 @@
 const logger = require('../common/logger');
 
+/**
+ * Lambda method responsible for receiving an user backup in JSON format
+ * and adding it to the user backup table
+ *
+ * @param event
+ * @param context
+ * @return {{body: string, statusCode: number}}
+ */
 const importBackup = (event, context) => {
   logger.debug(event);
   logger.debug(context);
@@ -9,6 +17,12 @@ const importBackup = (event, context) => {
   };
 };
 
+/**
+ *
+ * @param event
+ * @param context
+ * @return {{body: string, statusCode: number}}
+ */
 const exportBackup = (event, context) => {
   logger.debug(event);
   logger.debug(context);
