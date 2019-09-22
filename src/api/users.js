6d7f587024f840e0c408296e18f1da572502b92d
Daniel Augusto de Melo Santos <danielmelos@gmail.com>
2019-09-22 15:20:17 -0300

    Restructure coding for backup endpoints

--- src/api/users.js
+++ src/api/users.js
@@ -1,52 +1,24 @@
-const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const logger = require('../common/logger');
 
-const signUp = async (event) => {
-  console.log(process.env)
-  const poolData = {
-    UserPoolId: process.env.COGNITO_POOL_USER_ID,
-    ClientId: process.env.COGNITO_USER_CLIENT_ID,
+const importBackup = (event, context) => {
+  logger.debug(event);
+  logger.debug(context);
+  return {
+    statusCode: 200,
+    body: 'HELLO',
   };
-  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-  const payload = event.body;
-
-  const attributeList = [];
-
-  const dataEmail = {
-    Name: 'email',
-    Value: payload.email,
-  };
-  const dataPhoneNumber = {
-    Name: 'phone_number',
-    Value: payload.phone,
-  };
-  const attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-  const attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber);
-
-  attributeList.push(attributeEmail);
-  attributeList.push(attributePhoneNumber);
-
-  userPool.signUp('username', 'password', attributeList, null, function(err, result){
-    if (err) {
-      alert(err.message || JSON.stringify(err));
-      return;
-    }
-    var cognitoUser = result.user;
-    console.log('user name is ' + cognitoUser.getUsername());
-
-    return {
-      statusCode: 200,
-    };
-  });
-
-
 };
 
-// Private API
-module.exports.privateEndpoint = (event, context) => ({
-  statusCode: 403,
-});
+const exportBackup = (event, context) => {
+  logger.debug(event);
+  logger.debug(context);
+  return {
+    statusCode: 200,
+    body: 'HELLO',
+  };
+};
 
 module.exports = {
-  signUp,
+  importBackup,
+  exportBackup,
 };
